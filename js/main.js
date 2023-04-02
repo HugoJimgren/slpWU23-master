aa84a3ce5c0f1c9378a87bc2f93c3da8a844bbe9
HugoJimgren <hugo.jimgren@elev.ga.ntig.se>
2023-04-02 23:46:28 +0200

    färdig

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@ let layer = document.querySelector(".fade-layer");
 layer.addEventListener("click",showMenu)
 
 let button = document.querySelector(".menu-button");
-button.addEventListener("click",showMenu)
+if (button){
+    button.addEventListener("click",showMenu)
+}
 
+let button2 = document.querySelector(".menu-button2");
+if (button2){
+    button2.addEventListener("click",showMenu)
+}
 
 
 function showMenu(){
@@ -15,31 +21,65 @@ function showMenu(){
 
     let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible")
+
 }
+
+/* FOTO */
+
+let imgButton = document.querySelector("#foto1")
+if (imgButton){
+    imgButton.addEventListener("click",enlarge)
+}
+
+function enlarge(){
+    let enlarged = document.querySelector(".foto1_enlarged")
+    enlarged.classList.toggle(".foto_enlarged")
+    console.log('a')
+}
+
 
 /* UTRUSTNING */
 
-let camButton = document.querySelector(".cam-button");
-button.addEventListener("click",camImage)
+let camButton = document.querySelector(".cam_button");
+if (camButton){
+    camButton.addEventListener("click",camImage)
+}
 
-let filmButton = document.querySelector(".film-button");
-button.addEventListener("click",filmImage)
+let filmButton = document.querySelector(".film_button");
+if (filmButton){
+    filmButton.addEventListener("click",filmImage)
+}
 
-let lensButton = document.querySelector(".lens-button");
-button.addEventListener("click",lensImage)
+let lensButton = document.querySelector(".lens_button");
+if (lensButton){
+    lensButton.addEventListener("click",lensImage)
+}
+
 
 
 function camImage(){
-    let next = document.querySelector("");
-    menu.classList.toggle("show");
+    let camImg = document.querySelector(".gear_img1");
+    camImg.classList.toggle("img_show");
+    let lensImg = document.querySelector(".gear_img3");
+    lensImg.classList.remove("img_show");
+    let filmImg = document.querySelector(".gear_img2");
+    filmImg.classList.remove("img_show");
 }
 
 function filmImage(){
-    let next = document.querySelector("");
-    menu.classList.toggle("show");
+    let camImg = document.querySelector(".gear_img1");
+    camImg.classList.remove("img_show");
+    let lensImg = document.querySelector(".gear_img3");
+    lensImg.classList.remove("img_show");
+    let filmImg = document.querySelector(".gear_img2");
+    filmImg.classList.toggle("img_show");
 }
 
 function lensImage(){
-    let next = document.querySelector("");
-    menu.classList.toggle("show");
+    let camImg = document.querySelector(".gear_img1");
+    camImg.classList.remove("img_show");
+    let lensImg = document.querySelector(".gear_img3");
+    lensImg.classList.toggle("img_show");
+    let filmImg = document.querySelector(".gear_img2");
+    filmImg.classList.remove("img_show");
 }
